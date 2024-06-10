e3f633de74bd6afa59b24abf7537ed74acf2dca7
Chris <sdecuir12@gmail.com>
2024-06-10 17:24:22 -0500

    modify function that gets forecast data by putting all data in one place and add docEl function to grab all elements on the page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,62 @@
 import './style.css';
 
+const docEl = (function() {
+  let input = document.querySelector('input');
+  const todayDate = document.getElementById('today\'s date');
+  const todayTemp = document.getElementById('Temp');
+  const todayRain = document.getElementById('Rain');
+  const todayLow = document.getElementById('Daily Low');
+  const todayHigh = document.getElementById('Daily High');
+  
+  const tomorrowDate = document.getElementById('today\'s date');
+  const tomorrowTemp = document.getElementById('Temp');
+  const tomorrowRain = document.getElementById('Rain');
+  const tomorrowLow = document.getElementById('Daily Low');
+  const tomorrowHigh = document.getElementById('Daily High');
+  
+  const dayAfterDate = document.getElementById('today\'s date');
+  const dayAfterTemp = document.getElementById('Temp');
+  const dayAfterRain = document.getElementById('Rain');
+  const dayAfterLow = document.getElementById('Daily Low');
+  const dayAfterHigh = document.getElementById('Daily High');
+  const feelsLike = document.querySelector('#feelsLike');
+  const windSpeed = document.querySelector('#windSpeed');
+  const gust = document.querySelector('#gust');
+  const windChill = document.querySelector('#windChill');
+  const precipitation = document.querySelector('#precipitation');
+  const country = document.querySelector('#country');
+  const city = document.querySelector('#city');
+  const region = document.querySelector('#region');
+  const humidity = document.querySelector('#humidity');
 
-let input = 'paris';
+  return{
+    input,    
+    todayDate,
+    todayTemp,
+    todayRain,
+    todayLow,
+    todayHigh,
+    tomorrowDate,
+    tomorrowTemp,
+    tomorrowRain,
+    tomorrowLow,
+    tomorrowHigh,
+    dayAfterDate,
+    dayAfterTemp,
+    dayAfterRain,
+    dayAfterLow,
+    dayAfterHigh,
+    feelsLike,
+    windSpeed,
+    gust,
+    windChill,
+    precipitation,
+    country,
+    city,
+    region,
+    humidity
+  }
+})();
 
 async function logWeather(location) {
   let query = location;
@@ -21,6 +76,9 @@ async function logWeather(location) {
   let humidity = cur.humidity;
   let windMPH = cur.wind_mph;
   let windKPH = cur.wind_kph;
+  let gustMPH = cur.gust_mph;
+
+
 
 
     return {
@@ -34,10 +92,78 @@ async function logWeather(location) {
     feelF,
     humidity,
     windKPH,
-    windMPH}
+    windMPH,
+    gustMPH
+
+  }
+}
+
+async function logForecast(location) {
+  let query = location;
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=7696cbc4355444c4bc1221226240606&q=${query}&days=3`, {mode: 'cors'});
+  let data = await response.json();
+  let today = data.forecast.forecastday[0].day;
+  let tomorrow = data.forecast.forecastday[1].day;
+  let overmorrow = data.forecast.forecastday[2].day;
+
+  const todayPrecip = today.daily_chance_of_rain;
+  const tomPrecip = tomorrow.daily_chance_of_rain;
+  const overPrecip = overmorrow.daily_chance_of_rain;
+  const todayDate = data.forecast.forecastday[0].date;
+  const tomDate = data.forecast.forecastday[1].date;
+  const overDate = data.forecast.forecastday[2].date;
+  const todayHigh = today.maxtemp_F;
+  const tomHigh = tomorrow.maxtemp_F;
+  const overHIgh = overmorrow.maxtemp_F;
+  const todayLow = today.mintemp_F;
+  const tomLow = today.mintemp_F;
+  const overLow = overmorrow.mintemp_F;
+
+
+console.log(data)
+
+    return {
+      data,
+      todayPrecip,
+      tomPrecip,
+      overPrecip,
+      todayDate,
+      tomDate,
+      overDate,
+      todayHigh,
+      tomHigh,
+      overHIgh,
+      todayLow,
+      tomLow,
+      overLow
+}
 }
 
 
-let foo = logWeather('paris').then(resolve =>  {console.log(resolve.country)});
 
-console.log(foo)
+
+let foo1 = logForecast('paris').then(resolve =>  {console.log(resolve.data.forecast.forecastday[0].day)});
+
+
+docEl.input.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    logWeather(docEl.input.value).then(resolve =>  {
+      console.log(docEl.feelsLike);
+      PrintPage(resolve)
+      docEl.input.value = '';
+    })
+  }
+});
+
+
+function PrintPage (input) {
+  console.log(input.country)
+  docEl.feelsLike.textContent = `${input.country}`;
+}
+
+(function ()  {
+const foo = document.querySelector('header')
+const bar = document.querySelector('#middle')
+const weekly = document.querySelector('.weekly') 
+weekly.style.marginTop = window.innerHeight - foo.offsetHeight - bar.offsetHeight - weekly.offsetHeight - 50 + 'px'
+})();
